80a282b0a21195a255c7e882cd9b9f993388703a
ivandrosilva12 <ivandrosilva@gmail.com>
2022-08-01 23:27:37 +0100

    Init CI workflow

---
+++ db/query/entry.sql
@@ -0,0 +1,26 @@
+-- name: CreateEntry :one
+INSERT INTO entries (
+  account_id,
+  amount
+) VALUES (
+  $1, $2
+) RETURNING *;
+
+-- name: GetEntry :one
+select * from entries
+where id = $1 limit 1;
+
+-- name: ListEntries :many
+select * from entries
+order by id
+limit $1
+offset $2;
+
+-- name: UpdateEntry :one
+update entries
+set  amount = $2
+where id = $1
+returning *;
+
+-- name: DeleteEntry :exec
+delete from entries where id = $1;
